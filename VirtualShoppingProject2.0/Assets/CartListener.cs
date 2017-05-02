a21d1c30f1e012c3bc605b2c8779c74b6ddcbaa8
bakunin95 <jugle66@hotmail.com>
2017-05-02 17:07:14 -0400

    project

---
+++ VirtualShoppingProject2.0/Assets/CartListener.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartListener : MonoBehaviour {
+
+
+
+    public List<Product> productList;
+
+    // Use this for initialization
+    void Start () {
+        productList = new List<Product>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //Product product
+    public void addToCart(Product product) {
+
+        Debug.Log("Adding a product");
+
+
+        Debug.Log("Adding a product"+product.id);
+
+
+        if (productList.Contains(product))
+        {
+            Debug.Log("Product already exist");
+
+        }
+        else {
+            Debug.Log("Adding a product");
+            productList.Add(product);
+        }
+
+        
+    }
+
+
+
+
+}
+
+
+
+
---
+++ VirtualShoppingProject2.0/Assets/Product.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Product : MonoBehaviour {
+
+
+   
+    public long id = 1;
+    public long qty = 1;
+
+    public Product(long id, long qty)
+    {
+
+        this.id = id;
+        this.qty = qty;
+
+    }
+
+  
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+
+        Debug.Log("Something collided");
+
+
+        //Product prodTest = new Product(1,1);
+
+
+        CartListener cart = GameObject.Find("CartListener").GetComponent<CartListener>();
+
+        cart.addToCart(this);
+
+        //transform.gameObject.GetComponent<Product>();
+
+        if (col.gameObject.layer == 10)
+        {
+
+            Debug.Log("Object Added to cart");
+
+         
+        }
+    }
+
+    /*
+    void OnCollisionStay(Collision col)
+    {
+
+        Debug.Log("Something collided");
+
+
+        Destroy(transform.gameObject);
+
+        if (col.gameObject.layer == 10)
+        {
+
+            Debug.Log("Object Added to cart");
+
+          
+        }
+    }*/
+
+
+}
