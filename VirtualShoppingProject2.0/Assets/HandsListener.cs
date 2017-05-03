11693d0c33c7fc264de82fd14401f969b2e52df5
bakunin95 <jugle66@hotmail.com>
2017-05-03 18:16:39 -0400

    hands

---
+++ VirtualShoppingProject2.0/Assets/HandsListener.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandsListener : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        Debug.Log("Hand Listener");
+
+        Debug.Log(other.gameObject.name);
+
+        Debug.Log(other.gameObject.layer);
+
+        if (other.gameObject.layer == 10)
+        {
+            other.gameObject.transform.SetParent(this.gameObject.transform);
+
+
+            other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+
+
+            //other.gameObject.transform.localPosition = new Vector3(1.25f,0,0);
+        }
+
+        
+    }
+
+
+}
