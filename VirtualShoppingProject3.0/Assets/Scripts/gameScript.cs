306bce6a6aceaea6237278bff713d021350fb258
bakunin95 <jugle66@hotmail.com>
2017-07-20 17:15:19 -0400

    market and bowling

---
+++ VirtualShoppingProject3.0/Assets/Scripts/gameScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameScript : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject[] gameobjectArray;
+	private Vector3[] gameobjectInitialPositionArray = new Vector3[100];
+	private int score = 0;
+	void Start () {
+		initialPosition ();
+		Debug.Log("gameobjectArray: "+gameobjectArray.Length);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (1)) {
+			calculateScore ();
+			resetPosition ();
+		}
+			
+	}
+
+	public void initialPosition(){
+
+		for(int i=0; i<gameobjectArray.Length; i++)
+		{
+			Debug.Log(i);
+			Debug.Log("gameobjectArray: "+gameobjectArray [i].transform.position);
+			gameobjectInitialPositionArray [i] = gameobjectArray [i].transform.position;
+		}
+
+	}
+
+	public void resetPosition(){
+
+		for(int i=0; i<gameobjectArray.Length; i++)
+		{
+			gameobjectArray [i].transform.position = gameobjectInitialPositionArray [i];
+			gameobjectArray [i].transform.rotation = Quaternion.identity;
+			//Vector3.zero;
+		}
+		score = 0;
+	}
+
+	public void calculateScore(){
+
+		for(int i=0; i<gameobjectArray.Length; i++)
+		{
+			//Debug.Log(gameobjectArray [i].transform.rotation.eulerAngles);
+
+			if ((gameobjectArray [i].transform.position.x != gameobjectInitialPositionArray [i].x && 
+				gameobjectArray [i].transform.position.z != gameobjectInitialPositionArray [i].z && 
+				gameobjectArray [i].transform.position.y != gameobjectInitialPositionArray [i].y) &&
+				(gameobjectArray [i].transform.rotation.eulerAngles.x >= 2f ||
+				gameobjectArray [i].transform.rotation.eulerAngles.y >= 2f ||
+					gameobjectArray [i].transform.rotation.eulerAngles.z >= 2f) 
+
+			)
+			{
+				//Debug.Log("in if");
+				score = score + 1;
+			}
+		}
+		Debug.Log("score is: "+score);
+	}
+
+}
---
+++ VirtualShoppingProject3.0/Assets/bowlingReset.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bowlingReset : MonoBehaviour {
+	public gameScript game;
+	// Use this for initialization
+
+	public float startCounter = 0;
+
+	public Vector3 initialPos;
+
+	void Start () {
+		startCounter = 0;
+		initialPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+		if (transform.localPosition.x < 89) {
+		
+			startCounter += Time.deltaTime;
+			
+		
+		}
+
+		if (startCounter > 5) {
+
+			//Debug.Log (startCounter);
+			Debug.Log (startCounter);
+
+
+			game.calculateScore();
+			game.resetPosition ();
+			transform.position = initialPos;
+			startCounter = 0f;
+		}
+		//if(startCounter - Time.deltaTime ()
+
+	}
+}
